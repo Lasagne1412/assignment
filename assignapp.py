ab705286204f65576bb67cd977d32cf0e413b9f0
Lasagne1412 <94594132+Lasagne1412@users.noreply.github.com>
2021-11-24 23:31:41 +0800

    Update assignapp.py

--- assignapp.py
+++ assignapp.py
@@ -103,7 +103,9 @@ else:
     st.header('Model Prediction')
     st.write('Please insert appropriate values for prediction')
     st.write('Platform')
-    #platchoice=st.selectbox('Platform select',['PS','PS2','PS3','PS4','PSV','XB','PC','XOne','DC','GC','Wii','WiiU])
+    platchoice=st.selectbox('Platform select',['PS','PS2','PS3','PS4','PSV','XB','PC','XOne','DC','GC','Wii','WiiU])
+    st.write(platchoice)
+   
     #['PS','PS2','PS3','PS4','PSV','XB','PC','XOne','DC','GC','Wii','WiiU']
     #st.write('Publisher')
     #pubchoice=st.selectbox('Nintendo','Microsoft Game Studios','Sony Computer Entertainment','Activision')
